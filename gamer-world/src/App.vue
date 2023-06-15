e8a1df545caaef029ba2f55770e835870ada6d24
Ezequiel <exequieldiego@gmail.com>
2023-06-15 19:11:33 -0300

    added navbar

--- gamer-world/src/App.vue
+++ gamer-world/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <NavBar/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NavBar
   }
 }
 </script>
---
+++ gamer-world/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <nav class="encabezado__barra-navegacion">
+            <ul>
+                <li><a class="nav-link" href="./Categorias/Gabinetes/gabinete.html">Gabinetes</a></li>
+                <li><a class="nav-link" href="./Categorias/Placas De Video/placadevideo.html">Placas De Video</a></li>
+                <li><a class="nav-link" href="./Categorias/Monitores/monitor.html">Monitores</a></li>
+                <li><a class="nav-link" href="./Categorias/Placa Madre/placamadre.html">Placa Madre</a></li>
+                <li><a class="nav-link" href="./Categorias/Procesadores/procesadores.html">Procesadores</a></li>
+                <li><a class="nav-link" href="./Categorias/Perifericos/perifiricos.html">Perifericos</a></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+  
+<script>
+  export default {
+    name: 'NavBar',
+    
+  }
+</script>
+  
+<style scoped>
+  
+</style>
